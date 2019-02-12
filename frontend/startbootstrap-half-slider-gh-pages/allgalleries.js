3ef5c71fba5d5e5a23e33674ce83c316416e10a1
anitachandlermohan <anitachandlermohan@gmail.com>
2019-02-12 17:59:05 +0000

    edited styling of galleries page

---
+++ frontend/startbootstrap-half-slider-gh-pages/allgalleries.js
@@ -0,0 +1,74 @@
+let piece_array = [];
+let gallery_array = [];
+let searchResults = [];
+
+function loadGalleryDB(){
+    let xhttp = new XMLHttpRequest();
+    let url = "http://localhost:8080/api/Gallery";
+    xhttp.onreadystatechange = function(){
+        if(xhttp.readyState ==4 && xhttp.status == 200){
+            let gallery_list = JSON.parse(xhttp.responseText);
+            for(let i in gallery_list){
+                document.getElementById("gallerycards").innerHTML += "<div class = 'card mb-3'>" 
+                +  "<img class = 'card-img-top' src=" + gallery_list[i].imageRef + ">"
+                + "<div class='card-body'><h1 class='card-text'>" + gallery_list[i].name + "</h1></div>";
+            }
+        }
+    }
+    xhttp.open("GET", url, true);
+    xhttp.setRequestHeader('Content-Type', 'application/json');
+    xhttp.setRequestHeader('Access-Control-Allow-Origin', '*');
+    
+    xhttp.send();
+
+}
+
+function getURLparams(variable){
+    let query = window.location.search.substring(1);
+    let urlparam = query.split("&");
+    for (let i = 0; i <urlparam.length; i++){
+        let keyvaluepair = urlparam[i].split("=");
+        if(keyvaluepair[0] == variable){
+            return keyvaluepair[1];
+        }
+    }
+    return(false);
+
+}
+function getGalleryByID(){
+    let galleryID = getURLparams("gallery");
+    let xhttp = new XMLHttpRequest();
+
+    let url = "http://localhost:8080/api/Gallery/" + galleryID;
+
+    xhttp.open("GET", url, true);
+    xhttp.setRequestHeader('Content-Type', 'application/json');
+    xhttp.setRequestHeader('Access-Control-Allow-Origin', '*');
+    xhttp.send();
+ 
+    xhttp.onload = function(){
+         let gallery = JSON.parse(xhttp.responseText);
+            let galleryName = gallery.name;
+            let galleryCountry = gallery.country;
+            let galleryCity = gallery.city;
+            let galleryImage = gallery.imageRef;
+            let galleryDesc = gallery.description;
+            generateGalleryPage(galleryName,galleryCity,galleryCountry,galleryImage,galleryDesc);
+    }
+}
+
+
+
+function passSearchParameters(){
+    
+    let searchTerm = document.getElementById("searchTerm").value;
+    let querystring = "?search=" + searchTerm;
+    let newURL = "index.html" + querystring;
+    console.log(newURL);
+    return newURL;
+}
+
+function presentAllGalleries(){
+
+}
+   
